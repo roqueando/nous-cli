ce0dfcaf61c3371a603e0806fc927a119dae7405
Vitor Roque <vitor.roque@netshow.me>
2020-04-11 18:37:36 -0300

    feat: demonter command

---
+++ src/commands/demonter/index.ts
@@ -0,0 +1,62 @@
+import {Command, flags} from '@oclif/command'
+import {Token} from '@roqueando/nous';
+import {createConnection} from 'net';
+import chalk from 'chalk';
+import boxen from 'boxen';
+//@ts-ignore
+import ansiAlign = require('ansi-align');
+//@ts-ignore
+import cfonts = require('cfonts');
+import * as fs from 'fs';
+import {spawn} from 'child_process';
+import * as path from 'path';
+
+export default class Demonter extends Command {
+
+  static description = 'closes all services and the manager'
+
+  static examples = [
+    `$ nous demonter 8080 KEY`,
+  ];
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+  }
+
+  static args = [
+    {
+      name: 'port',
+      key: 'key'
+    }
+  ];
+
+  async run() {
+    const { args, flags } = this.parse(Demonter);
+    const client = createConnection({ port: args.port ? args.port : 8080  });
+
+    client.write(JSON.stringify({
+      action: 'down',
+      payload: {
+        from: new Token(args.key ? args.key : null).getToken(),
+      }
+    }));
+    const output = cfonts.render('nous', {
+      colors: ['white'],
+      gradient: ["#DAE2F8", "#D6A4A4"],
+      space: false,
+    });
+    const boxer = boxen(
+      output.string +
+        "\n\n" + chalk.dim.bgBlackBright.magentaBright("      closing manager and services      "),
+      {padding: 3}
+    );
+    
+    client.on('data', payload => {
+      if(payload.toString() === 'OK') {
+        const pid = fs.readFileSync(path.resolve(__dirname + '../../../../tmp/main.pid'), 'utf8');
+        process.kill(parseInt(pid), 'SIGTERM');
+      }
+    });
+    this.log(boxer);
+  }
+}
